a1507d766ba301daba2d7195a653d8e092245c2c
buikevin96 <buikevin96@gmail.com>
2018-08-03 12:52:44 -0400

    Removed original django installation and reinstalled. Successfully started Django

--- pro/todo/todo/settings.py
+++ pro/todo/todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'di=_^e+2fw+p!q)92k3(1i3944or+zzr2%=-4m2+)441vr0#@w'
+SECRET_KEY = '1ieug*zs*e*4#=jujnf_jb=rs=i7=#+=qdxzcg9w=l)frr@lqa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
