3a961765727c424800775514f53838887d380da5
buikevin96 <buikevin96@gmail.com>
2018-08-03 22:58:51 -0400

    Created the ability to use the link to connect to server todoapp/

--- pro/todo/todoapp/urls.py
+++ pro/todo/todoapp/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include, url
+from todoapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('todoapp/', include('todoapp.urls')),
+    # path('admin/', admin.site.urls),
+    url(r'^$', views.index,name='index'),
 ]
