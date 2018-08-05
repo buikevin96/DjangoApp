78dc63718a566e9079719d7b2c5f9595aa05fe57
buikevin96 <buikevin96@gmail.com>
2018-08-05 14:33:17 -0400

    Added front-end index.html to display task_list

--- pro/todo/todoapp/views.py
+++ pro/todo/todoapp/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
     task_list = Task.objects.all() # Retrieve all the objects from the task model
-    output = ','.join([q.task_name for q in task_list])# Retrieve all of the task_names in the task_list and append a comma after them
-    return HttpResponse(output)
+    template = loader.get_template('todoapp/index.html')
+    context = {
+
+        'task_list': task_list
+    }
+    #output = ','.join([q.task_name for q in task_list])# Retrieve all of the task_names in the task_list and append a comma after them
+    return HttpResponse(template.render(context, request))
 
 def detail(request, task_id):
     return HttpResponse('This is task number %s' % task_id)
+
 
